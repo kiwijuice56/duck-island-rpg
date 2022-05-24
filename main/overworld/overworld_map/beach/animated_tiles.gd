3eb240d2986b6d604aac5230ed1b3ecb1d6eedf4
kiwijuice56 <alfaroeric127@gmail.com>
2022-05-23 21:55:07 -0500

    Improved water animations

--- main/overworld/overworld_map/beach/animated_tiles.gd
+++ main/overworld/overworld_map/beach/animated_tiles.gd
@@ -4,22 +4,14 @@ export var frames: int
 export var size: int
 
 var offset := Vector2()
-var returning = false
 
 func _ready() -> void:
 	$Timer.connect("timeout", self, "next_frame")
 
 func next_frame() -> void:
-	if not returning:
-		offset.x += size/frames
-	else:
-		offset.x -= size/frames
-	if offset.x < 0:
-		returning = false
-		offset.x += 2*size/frames
+	offset.x += size/frames
 	if offset.x >= size:
-		returning = true
-		offset.x -= 2*size/frames
+		offset.x = 0
 	var rect = get_tileset().tile_get_region(0)
 	rect.position = offset
 	get_tileset().tile_set_region(0, rect)
